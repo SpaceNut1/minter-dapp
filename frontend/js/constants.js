a6657b7c188ffa9359bd26844f9b9edc45b2dd2f
SpaceNut1 <102760669+SpaceNut1@users.noreply.github.com>
2022-04-04 19:38:08 -0700

    icon

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,21 +1,21 @@
 const contractAddress = "0x2e3B959F382E86455E129376ce28A3766faF5142"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "This is The Space Nuts Invaders NFT Project!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_p = 'The Space Nuts Invaders NFT is dedicated to building its community and utility until we reach our goal of a MetaSpace VR that affordable and profitable; not just for those who pay but for those who play. Join our discord community to find out when the Nuts drop. <a href="https://discord.gg/t5hWJ6g"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const h1_presale_coming_soon = "NUTs Dropping Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_coming_soon = "Space Nuts Invaders landing on your blockchain soon. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_whitelisted = "Mint your Space Nut";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
